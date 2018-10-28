fbf8477a72dc6b8dc7ac836b75f716ab837c1a86
Simone Basso <bassosimone@gmail.com>
2018-10-28 22:52:57 +0100

    Remove mkall/vendor: for now we'll use MK for that

--- mkall/vendor/mkdata.cpp
+++
@@ -1,6 +0,0 @@
-// Part of Measurement Kit <https://measurement-kit.github.io/>.
-// Measurement Kit is free software under the BSD license. See AUTHORS
-// and LICENSE for more information on the copying conditions.
-
-#define MKDATA_INLINE_IMPL
-#include "mkdata.h"
