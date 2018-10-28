d77e7bf058f47a39afb6734e06510d19f67e282c
Simone Basso <bassosimone@gmail.com>
2018-10-28 19:57:25 +0100

    Add tests for mkcurl

--- mkallTests/mkallTests.m
+++ mkallTests/mkallTests.m
@@ -12,23 +12,10 @@
 
 @implementation mkallTests
 
-- (void)setUp {
-}
-
-- (void)tearDown {
-}
-
 - (void)testMkResources {
   XCTAssert([MkResources getMMDBCountryPath] != nil);
   XCTAssert([MkResources getMMDBASNPath] != nil);
   XCTAssert([MkResources getCABundlePath] != nil);
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
 }
 
 @end
