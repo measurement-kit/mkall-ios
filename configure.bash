1c0dd003f46553713b5305b998758dca2c8aa2e0
Simone Basso <bassosimone@gmail.com>
2018-12-03 12:51:39 +0100

    Make sure we download the most recent assets

--- configure.bash
+++ configure.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -ex
-prebuilt_repo=https://github.com/measurement-kit/prebuilt
 
 git submodule update --init  # Just in case
 
@@ -11,13 +10,13 @@ if [ ! -f Frameworks/.stamp ]; then (
   touch Frameworks/.stamp
 ) fi
 
-# TODO(bassosimone): this should perhaps be a release of the assets themselves.
 # Download generic assets (.mmdb files, ca-bundle.pem)
 if [ ! -f mkall/resources/.stamp ]; then (
   set -ex
-  version=20181028
-  shasum=caffe9d811b87f6bb45a5011b58982844f5d3d126ba99339a31c0cbcda6b67b6
-  curl -fsSLO $prebuilt_repo/releases/download/testing/generic-assets-$version.tar.gz
+  version=20181203
+  shasum=01f7c8de625d8aae1239a1758097055a64aee60d69aadf0de372f204c36f35b5
+  repodload=https://github.com/measurement-kit/generic-assets/releases/download
+  curl -fsSLO $repodload/$version/generic-assets-$version.tar.gz
   real_shasum=`shasum -a 256 generic-assets-$version.tar.gz | awk '{print $1}'`
   [ $real_shasum = $shasum ]
   (
