a8fb55b38fde697bf016d46099aece6a3d14bfec
Simone Basso <bassosimone@gmail.com>
2019-05-30 15:59:44 +0200

    Use homebrew for getting dependencies
    This commit changes the development workflow to get dependencies
    using our homebrew tap. This reduces the number of custom scripts
    that one needs to run to develop for iOS.

--- configure.bash
+++ configure.bash
@@ -1,30 +1,5 @@
 #!/bin/bash
 set -ex
-
-git submodule update --init  # Just in case
-
-# Download all MK libraries in a single archive
-if [ ! -f Frameworks/.stamp ]; then (
-  ./script/build/ios/make-ios-frameworks
-  rm -rf MK_DIST  # cleanup
-  touch Frameworks/.stamp
-) fi
-
-# Download generic assets (.mmdb files, ca-bundle.pem)
-if [ ! -f mkall/resources/.stamp ]; then (
-  set -ex
-  version=20190327
-  shasum=bbff2710b89fe22611e6dd48d401064d02bd0232da72dde29fc8c2131abd33b7
-  repodload=https://github.com/measurement-kit/generic-assets/releases/download
-  curl -fsSLO $repodload/$version/generic-assets-$version.tar.gz
-  real_shasum=`shasum -a 256 generic-assets-$version.tar.gz | awk '{print $1}'`
-  [ $real_shasum = $shasum ]
-  (
-    set -ex
-    rm -f mkall/resources/*
-    cd mkall/resources
-    tar -xzf ../../generic-assets-$version.tar.gz
-  )
-  rm -f generic-assets-$version.tar.gz
-  touch mkall/resources/.stamp
-) fi
+homebrew_basedir="/usr/local/opt"
+./script/configure/update-frameworks $homebrew_basedir
+./script/configure/update-resources $homebrew_basedir
